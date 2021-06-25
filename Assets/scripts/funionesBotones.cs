1292b98a62f28f61b3f7935f709b6d52f471668b
villegas545 <villegas_545@hotmail.com>
2021-06-24 22:05:12 -0600

    commit

---
+++ Assets/scripts/funionesBotones.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class funionesBotones : MonoBehaviour
+{
+    public void botonpresionado(GameObject personaje){
+
+        personaje.GetComponent<AudioSource>().clip = personaje.GetComponent<personaje>().audiopersonaje2;
+        personaje.GetComponent<AudioSource>().Play();
+        GameObject.FindGameObjectWithTag("segundocanvas").GetComponent<Canvas>().enabled = true;
+        GameObject.FindGameObjectWithTag("primercanvas").GetComponent<Canvas>().enabled = false;
+    }
+}
---
+++ Assets/scripts/personaje.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class personaje : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public AudioClip audiopersonaje,audiopersonaje2;
+
+    private bool bandera = false;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y<=0){
+            if(bandera==false){
+                bandera = true;
+                Debug.Log("hola"); 
+                GetComponent<AudioSource>().clip = audiopersonaje;
+                GetComponent<AudioSource>().Play();
+                GetComponent<Animator>().SetTrigger("hablar");
+            }
+        }else{
+            transform.position = new Vector3(transform.position.x, (transform.position.y-.3f), transform.position.z);
+        }
+    }
+}
